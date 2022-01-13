32f3fd072e411c805e93015cd6d9586c61b2d3cc
maycolroa <85509333+maycolroa@users.noreply.github.com>
2022-01-13 07:50:52 -0500

    monty.1

---
+++ pall.c
@@ -0,0 +1,10 @@
+#include "monty.h"
+
+/**
+ * pall - This function prints all the values on the stack,
+ * starting from the top's stack.
+ * @stack: Double pointer to the stacks's head
+ * @line_number: line number.
+ * Return:
+ */
+void pall(stack_t **stack, unsigned int line_number)
